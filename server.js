8338268a1f24b956024ade4d485ad3c0fb1854d8
Ellen Park <ellenpark781@gmail.com>
2021-08-24 16:39:40 -0400

    set up lmorgan, body-parser, view engine, and load assets

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express'); 
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express(); 
+dotenv.config({path:'config.env'})
+
+// log requests
+app.use(morgan('tiny')); 
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true})); 
+
+// set view engine 
+app.set("view engine", "ejs")
+// app.set("views", path.resolve(__dirname, "views/ejs"))
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, '/assets/css')))
+app.use('/img', express.static(path.resolve(__dirname, '/assets/img')))
+app.use('/js', express.static(path.resolve(__dirname, '/assets/js')))
 
 app.get('/', (req, res) => {
-    app.send("Hello World")
-});
-
-app.listen(3000, () => {
-    console.log("I am listening on port 3000")
+    res.send('Hello World')
 })
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`I am running on http://localhost:${PORT}`)
+}); 
