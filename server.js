cf12c54dc31f0fc50563c9f4e6db044d9b7a4654
Ellen Park <ellenpark781@gmail.com>
2021-08-26 16:05:10 -0400

    created an update user view and deleted HTML files that have been transferred to ejs files

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const bodyparser = require('body-parser');
 const path = require('path');
 
 const app = express(); 
+
 dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080
+
 
 // log requests
 app.use(morgan('tiny')); 
@@ -18,15 +21,22 @@ app.set("view engine", "ejs")
 // app.set("views", path.resolve(__dirname, "views/ejs"))
 
 // load assets
-app.use('/css', express.static(path.resolve(__dirname, '/assets/css')))
-app.use('/img', express.static(path.resolve(__dirname, '/assets/img')))
-app.use('/js', express.static(path.resolve(__dirname, '/assets/js')))
+
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
 app.get('/', (req, res) => {
     res.render('index');
 })
 
-const PORT = process.env.PORT || 8080
+app.get('/add-user', (req, res) => {
+    res.render('add_user');
+})
+
+app.get('/update-user', (req, res) => {
+    res.render('update_user')
+})
 
 app.listen(PORT, () => {
     console.log(`I am running on http://localhost:${PORT}`)
